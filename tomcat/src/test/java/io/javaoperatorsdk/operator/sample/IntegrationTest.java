304ac2a368d53779d53864cfeee4ee065a370364
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-08-12 18:42:27 +0200

    improving the code (untilAsserted)

--- tomcat/src/test/java/io/javaoperatorsdk/operator/sample/IntegrationTest.java
+++ tomcat/src/test/java/io/javaoperatorsdk/operator/sample/IntegrationTest.java
@@ -1,28 +1,27 @@
 package io.javaoperatorsdk.operator.sample;
 
-import io.fabric8.kubernetes.api.model.KubernetesResourceList;
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
-import io.fabric8.kubernetes.client.utils.Serialization;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.hamcrest.CoreMatchers.*;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class IntegrationTest {
+
+    final static String TEST_NS = "tomcat-test";
+
     @Test
     public void test() {
         Config config = new ConfigBuilder().withNamespace(null).build();
@@ -34,38 +33,35 @@ public class IntegrationTest {
 
         operator.register(new WebappController(client));
 
-        Tomcat tomcat = loadYaml(Tomcat.class, "k8s/tomcat-sample1.yaml");
-
+        Tomcat tomcat = new Tomcat();
+        tomcat.setMetadata(new ObjectMetaBuilder()
+                .withName("test-tomcat1")
+                .withNamespace(TEST_NS)
+                .build());
+        tomcat.setSpec(new TomcatSpec());
         tomcat.getSpec().setReplicas(3);
-        tomcat.getMetadata().setNamespace("tomcat-test");
+        tomcat.getSpec().setVersion(9);
 
         MixedOperation<Tomcat, KubernetesResourceList<Tomcat>, Resource<Tomcat>> tomcatClient = client.customResources(Tomcat.class);
 
         Namespace testNs = new NamespaceBuilder().withMetadata(
-                new ObjectMetaBuilder().withName("tomcat-test").build()).build();
+                new ObjectMetaBuilder().withName(TEST_NS).build()).build();
 
         // We perform a pre-run cleanup instead of a post-run cleanup. This is to help with debugging test results
         // when running against a persistent cluster. The test namespace would stay after the test run so we can
         // check what's there, but it would be cleaned up during the next test run.
         client.namespaces().delete(testNs);
 
-        await().atMost(300, SECONDS).until(() -> client.namespaces().withName("tomcat-test").get() == null);
+        await().atMost(5, MINUTES).until(() -> client.namespaces().withName("tomcat-test").get() == null);
 
         client.namespaces().createOrReplace(testNs);
 
         tomcatClient.inNamespace("tomcat-test").create(tomcat);
 
-        await().atMost(60, SECONDS).until(() -> {
+        await().atMost(2, MINUTES).untilAsserted(() -> {
             Tomcat updatedTomcat = tomcatClient.inNamespace("tomcat-test").withName("test-tomcat1").get();
-            return updatedTomcat.getStatus() != null && updatedTomcat.getStatus().getReadyReplicas() == 3;
+            assertThat(updatedTomcat.getStatus(), is(notNullValue()));
+            assertThat(updatedTomcat.getStatus().getReadyReplicas(), equalTo(3));
         });
-    }
-
-    private <T> T loadYaml(Class<T> clazz, String yaml) {
-        try (InputStream is = new FileInputStream(yaml)) {
-            return Serialization.unmarshal(is, clazz);
-        } catch (IOException ex) {
-            throw new IllegalStateException("Cannot find yaml on classpath: " + yaml);
-        }
     }
 }
